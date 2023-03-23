9c90a6a5feb87c37b0a4ad131f197eec1734c7ae
nelsonto <nelson.c.to@gmail.com>
2023-03-23 15:58:01 -0700

    Add code to read measurements from TM-X5000.

--- meca_measure_v0.py
+++ meca_measure_v0.py
@@ -4,45 +4,64 @@ Created on Mon Mar 20 14:53:54 2023
 
 @author: NelsonChunManTo
 """
+## IP addresses:
+# Mecademic meca500 robot - 192.168.0.100
+# Omron Hawkeye Barcode Reader V430-F - 192.168.0.200, port 2001
+# Keyence TM-X5000 - 192.168.0.201, port 8601
 
 import mecademicpy.robot as mdr
 import socket
 import cv2
+import tkinter as tk
+from tkinter import messagebox
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 robot = mdr.Robot()
 
+#initalize variables
+wireDiameter = []
 tray = []
-for i in range(10):
-    tray.append(tuple((179.350006,-130.494995+(i*20.183333),142,0,90,0)))
-        
+
+# connect to meca500
 def connectRobot():
     robot.Connect(address='192.168.0.100', disconnect_on_exception=False, enable_synchronous_mode=True)
-    print('Connected to robot')  
+    print('Connected to robot') 
+
+# initialize robot
 def init():
     connectRobot()
     print('Initalizing Robot: Activate, home, set limits...')
     robot.ActivateRobot()
     robot.Home()
-    
-    # Pause execution until robot is homed.
-    robot.WaitHomed()
+    robot.WaitHomed() # Pause execution until robot is homed.
     print('Robot is homed and ready.')
     
     returnHome()
     robot.SetGripperForce(30)
     robot.SetGripperRange(0,8)
-    robot.GripperOpen()   
+    robot.GripperOpen()
+    initTrayPositions()
+
+def initTrayPositions():
+    global tray
+    for i in range(10):
+        tray.append(tuple((179.350006,-130.494995+(i*20.183333),142,0,90,0)))
+    
 def returnHome():
     robot.MoveJoints(0,0,0,0,0,0)
     print('Robot is zeroed.') 
+    
 def disconnectRobot():
     returnHome()
     robot.DeactivateRobot()
     robot.Disconnect()
     print('Now disconnected from the robot.')
+    
 def resetRobot():
     robot.ResetError()
     robot.ResumeMotion()
+    
 def readBarcode():
     moveBarcode ()
     s = socket.socket()
@@ -50,8 +69,9 @@ def readBarcode():
     s.send('< >'.encode())
     print (s.recv(1024).decode())
     s.close()
+    
 def moveCircle (): 
-    movePickupWaypoint()
+    moveTrayWaypoint()
     robot.Delay(0.25)
     robot.MoveLin(180,-127,140,0,90,0) #top left
     robot.Delay(0.25)
@@ -72,32 +92,52 @@ def moveTrayWaypoint ():
     robot.MovePose(225,-38,150,0,90,0) #pickup waypoint
     
 def measureWires ():
-    moveMeasure()
-    for i in range(4):
-        robot.MoveLinRelTrf(0, 0, 9.5, 0, 0, 0)
-        print(i)
-        robot.Delay(0.25)
+    global wireDiameter 
+    
+    #moveMeasure()
+    s = socket.socket()
+    s.connect(('192.168.0.201', 8601))
+    s.send("GM,0,0\r".encode())
+    rawDiameter = s.recv(2048).decode("utf-8")
+    print("raw measurement is:", rawDiameter)
+    
+    wireDiameter = rawDiameter.split(',')
+    wireDiameter = wireDiameter [2:]
+    wireDiameter = [float(i) for i in wireDiameter]    #convert all strings to floats
+    wireDiameter = [i for i in wireDiameter if i > 0]  #filter out negative values (no measure from keyences shows up as -9999.99999 mm)
+    
+    print("Wire diameters are:", wireDiameter)
+    s.close()
+    
+    # for i in range(4):
+    #     robot.MoveLinRelTrf(0, 0, 9.5, 0, 0, 0)
+    #     print(i)
+    #     robot.Delay(0.25)
     
 def pickFx(i):
     robot.MovePose(*(tray[i]))
-    robot.MoveLinRelTrf(60, 0, 0, 0, 0, 0)
+    robot.MoveLinRelTrf(59, 0, 0, 0, 0, 0)
     robot.GripperClose()
     robot.MoveLinRelTrf(-65, 0, 0, 0, 0, 0)
     moveTrayWaypoint()
     
 def placeFx(i):
     robot.MovePose(*(tray[i]))
-    robot.MoveLinRelTrf(55, 0, 0, 0, 0, 0)
+    robot.MoveLinRelTrf(57, 0, 0, 0, 0, 0)
     robot.GripperOpen()
-    robot.MoveLinRelTrf(-55, 0, 0, 0, 0, 0)
+    robot.MoveLinRelTrf(-57, 0, 0, 0, 0, 0)
     moveTrayWaypoint()
     
 def pickNplace():
-    for i in range(3):
+    for i in range(10):
         pickFx(i)
         readBarcode()
         placeFx(i)
-        
-    
-# Pos 1 [179.350006,-130.494995,142,0,90,0]
-# Pos 10 [179.350006,51.154999,142,0,90,0]
+
+
+connectRobot()    
+init()
+x=0
+
+while x == 0:
+    pickNplace()
